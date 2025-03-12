0622f6ee2476196605937a9937d690476a18d110
paulalunanavarro <paulunnav@alum.us.es>
2025-03-13 00:27:09 +0100

    feat():Created Flight Crew Member in data model

---
+++ src/main/java/acme/entities/FlightCrewMember.java
@@ -0,0 +1,58 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+
+@Entity
+public class FlightCrewMember extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "[A-Z]{2-3}\\d{6}$")
+	@Automapped
+	private String				employeeCode;
+
+	@Mandatory
+	@Automapped
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	private String				phoneNumber;
+
+	@Mandatory
+	@Automapped
+	@ValidString(max = 255)
+	private String				languageSkills;
+
+	@Mandatory
+	@Automapped
+	@Valid
+	private AvailabilityStatus	availabilityStatus;
+
+	//Relatioship with airline entity
+	// @Mandatory
+	// @Automapped
+	//@Valid
+	// @ManyToOne(optional = false)
+	// private Airline				workingFor;
+
+	@Mandatory
+	@Automapped
+	@ValidMoney
+	private Money				salary;
+
+	@Optional
+	@Automapped
+	@ValidNumber
+	private Integer				yearsOfExperience;
+
+}
