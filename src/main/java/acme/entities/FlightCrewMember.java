ffca299d53c3e5998e070030f8166a4cf9c5f314
paulalunanavarro <paulunnav@alum.us.es>
2025-03-13 18:29:59 +0100

    feat(): adding Airline reationship in FlightCrewMember entity

--- src/main/java/acme/entities/FlightCrewMember.java
+++ src/main/java/acme/entities/FlightCrewMember.java
@@ -2,6 +2,7 @@
 package acme.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -12,6 +13,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airline.Airline;
 
 @Entity
 public class FlightCrewMember extends AbstractEntity {
@@ -38,12 +40,11 @@ public class FlightCrewMember extends AbstractEntity {
 	@Valid
 	private AvailabilityStatus	availabilityStatus;
 
-	//Relatioship with airline entity
-	// @Mandatory
-	// @Automapped
-	//@Valid
-	// @ManyToOne(optional = false)
-	// private Airline				workingFor;
+	@Mandatory
+	@Automapped
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				workingFor;
 
 	@Mandatory
 	@Automapped
