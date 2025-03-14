e4a75ecf396bf8bb743ed7ace445792ecdb23566
lucianoya <lucnoycan@alum.us.es>
2025-03-14 09:40:43 +0100

    D02-S2-R03:Store Customer Information

---
+++ src/main/java/acme/entities/customer/Customer.java
@@ -0,0 +1,52 @@
+
+package acme.entities.customer;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Pattern;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Automapped
+	private String				identifier;
+
+	@Mandatory
+	@Pattern(regexp = "^\\+?\\d{6,15}$", message = "Invalid phone number")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				physicalAddress;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				city;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				country;
+
+	@Optional
+	@ValidNumber(max = 500000)
+	@Automapped
+	private Integer				earnedPoints;
+}
